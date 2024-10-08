bc79db28456ca59dfb50b6b368126dc362f0a27c
Hamza <hj1087546@gmail.com>
2024-10-07 23:44:27 -0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,8 @@ Route::get('/vehicles-list', [App\Http\Controllers\VehiclesController::class, 'i
 Route::post('/vehicles-store', [App\Http\Controllers\VehiclesController::class, 'store'])->name('vehicles-store');
 Route::get('/vehicles-delete/{id}', [App\Http\Controllers\VehiclesController::class, 'delete'])->name('vehicles-delete');
 Route::post('/vehicles-update/{id}', [App\Http\Controllers\VehiclesController::class, 'update'])->name('vehicles-update');
+
+Route::get('/routes-modules-list', [App\Http\Controllers\VehiclesController::class, 'index'])->name('routes-modules-list');
+Route::post('/routes-modules-store', [App\Http\Controllers\VehiclesController::class, 'store'])->name('routes-modules-store');
+Route::get('/routes-modules-delete/{id}', [App\Http\Controllers\VehiclesController::class, 'delete'])->name('routes-modules-delete');
+Route::post('/routes-modules-update/{id}', [App\Http\Controllers\VehiclesController::class, 'update'])->name('routes-modules-update');
